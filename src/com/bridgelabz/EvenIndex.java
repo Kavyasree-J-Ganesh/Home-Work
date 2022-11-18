f86475ec7b8f72ae3d626b3786301f5b2ea98658
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-19 00:22:14 +0530

    [Kavya] Problem_2

--- src/com/bridgelabz/EvenIndex.java
+++ src/com/bridgelabz/EvenIndex.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class EvenIndex {
+    static void evenIndexElements(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            if(i % 2 == 0){
+                System.out.println(arr[i]);
+            }
+
+        }
+    }
     public static void main(String[] args) {
-        
+        int[] arr1 = {4,5,1,2,6,7,9,3};
+        evenIndexElements(arr1);
     }
 }
